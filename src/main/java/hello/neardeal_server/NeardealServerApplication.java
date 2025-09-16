3fa851a237022f646d6abbe7332e2134f837c5e7
dohyeon <ehgus8910@gmail.com>
2025-09-16 09:16:40 +0900

    INIT: Initial Commit

---
+++ src/main/java/hello/neardeal_server/NeardealServerApplication.java
@@ -0,0 +1,13 @@
+package hello.neardeal_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NeardealServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NeardealServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/hello/neardeal_server/NeardealServerApplicationTests.java
@@ -0,0 +1,13 @@
+package hello.neardeal_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NeardealServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
